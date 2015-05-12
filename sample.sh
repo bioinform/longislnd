1bde86e613eb57aed37961bac51af2d434798361
Bayo Lau <bayo.lau@gmail.com>
2015-05-12 16:44:18 -0700

    example scripts

---
+++ sample.sh
@@ -0,0 +1,14 @@
+
+HDF5_LIB=/Applications/HDFView.app/Contents/Resources/lib
+
+MODEL_PREFIX=model
+
+CMP_H5=/Users/bayo/Downloads/bax9_CHM1htert.cmp.h5
+
+BASE_FLANK=3
+
+MIN_LENGTH=1000
+
+FLANK_MASK=100
+
+java -Djava.library.path=${HDF5_LIB} -jar H5Test.jar sample ${MODEL_PREFIX} ${CMP_H5} ${BASE_FLANK} ${BASE_FLANK} ${MIN_LENGTH} ${FLANK_MASK}
---
+++ simulate.sh
@@ -0,0 +1,16 @@
+
+HDF5_LIB=/Applications/HDFView.app/Contents/Resources/lib
+
+MODEL_PREFIX=model
+
+OUTPUT_DIR=/Users/bayo/Downloads
+
+FASTA=/Users/bayo/Downloads/ecoli_mod.fasta
+
+NUM_BASES=235851050
+
+SAMPLE_PER=100
+
+SEED=1351
+
+java -Djava.library.path=${HDF5_LIB} -jar H5Test.jar simulate ${OUTPUT_DIR} ${FASTA} ${MODEL_PREFIX} ${NUM_BASES} ${SAMPLE_PER} ${SEED}
