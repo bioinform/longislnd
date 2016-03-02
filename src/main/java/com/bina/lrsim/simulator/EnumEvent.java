cad0e483ab9314d179769a83769cd9b1ace7e057
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-03-01 16:15:36 -0800

    cleanup enumevent

--- src/main/java/com/bina/lrsim/simulator/EnumEvent.java
+++ src/main/java/com/bina/lrsim/simulator/EnumEvent.java
@@ -15,32 +15,27 @@ import java.util.EnumSet;
  */
 public enum EnumEvent {
   // value, description, recording period, sample pool implementation
-  INSERTION(0, "i", Heuristics.SAMPLE_PERIOD_INS, KmerBCPool.class),
-  DELETION(1, "d", Heuristics.SAMPLE_PERIOD_DEL, DeletedSingleBCPool.class),
-  SUBSTITUTION(2, "s", Heuristics.SAMPLE_PERIOD_SUB, KmerBCPool.class),
-  MATCH(3, "m", Heuristics.SAMPLE_PERIOD_MAT, SingleBCPool.class);
+  INSERTION("i", Heuristics.SAMPLE_PERIOD_INS, KmerBCPool.class),
+  DELETION("d", Heuristics.SAMPLE_PERIOD_DEL, DeletedSingleBCPool.class),
+  SUBSTITUTION("s", Heuristics.SAMPLE_PERIOD_SUB, KmerBCPool.class),
+  MATCH("m", Heuristics.SAMPLE_PERIOD_MAT, SingleBCPool.class);
 
-  private static final EnumEvent[] value2enum_ = {INSERTION, DELETION, SUBSTITUTION, MATCH};
+  public static final EnumEvent[] values = values();
+
   public static String getListDescription() {
     StringBuilder sb = new StringBuilder();
-    sb.append(value2enum(0).toString());
+    sb.append(values[0].toString());
     for(int ii = 1; ii < EnumEvent.values().length; ++ii) {
-      sb.append(":"+value2enum(ii).toString());
+      sb.append(":"+values[ii]);
     }
     return sb.toString();
   }
 
-  public final int value;
   public final String description;
   public final int recordEvery;
   public final Class<?> pool;
 
-  public static EnumEvent value2enum(int i) {
-    return value2enum_[i];
-  }
-
-  EnumEvent(int value, String description, int recordPeriod, Class<?> pool) {
-    this.value = value;
+  EnumEvent(String description, int recordPeriod, Class<?> pool) {
     this.description = description;
     this.recordEvery = recordPeriod;
     this.pool = pool;
@@ -61,7 +56,7 @@ public enum EnumEvent {
   }
 
   public static String getPrettyStats(long[] data) {
-    if (data.length != EnumEvent.values().length) {
+    if (data.length != values.length) {
       return "invalid data length";
     }
     StringBuilder sb = new StringBuilder();
@@ -70,8 +65,8 @@ public enum EnumEvent {
       sum += entry;
     }
     for (EnumEvent ev : EnumSet.allOf(EnumEvent.class)) {
-      long count = data[ev.value];
-      sb.append(" " + ev.toString() + " " + count + String.format(" (%5.1f)", 100 * (double) (count) / sum));
+      long count = data[ev.ordinal()];
+      sb.append(" " + ev + " " + count + String.format(" (%5.1f)", 100 * (double) (count) / sum));
     }
     return sb.toString();
   }
