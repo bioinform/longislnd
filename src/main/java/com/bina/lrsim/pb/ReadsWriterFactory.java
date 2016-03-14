b404b45faeed4cce9e5dbe6cbe047173a1db8db1
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-03-14 15:55:29 -0700

    use htsjdk to check for BAM file name

--- src/main/java/com/bina/lrsim/pb/ReadsWriterFactory.java
+++ src/main/java/com/bina/lrsim/pb/ReadsWriterFactory.java
@@ -2,16 +2,16 @@ package com.bina.lrsim.pb;
 
 import com.bina.lrsim.pb.h5.bax.BaxH5Writer;
 import com.bina.lrsim.pb.bam.BAMWriter;
+import htsjdk.samtools.BamFileIoUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
 
 /**
  * Created by bayolau on 1/8/16.
  */
 public class ReadsWriterFactory {
   public static ReadsWriter makeWriter(PBSpec spec, String file_name, String movie_name, int first_hole) {
-    if (file_name.length() > 4 && file_name.substring(file_name.length() - 4).equals(".bam")) {
-      return new BAMWriter(spec, file_name, movie_name, first_hole);
-    } else {
-      return new BaxH5Writer(spec, file_name, movie_name, first_hole);
-    }
+    return BamFileIoUtils.isBamFile(new File(file_name)) ? new BAMWriter(spec, file_name, movie_name, first_hole) : new BaxH5Writer(spec, file_name, movie_name, first_hole);
   }
 }
