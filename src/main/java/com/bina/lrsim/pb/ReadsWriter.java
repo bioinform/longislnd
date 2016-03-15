66b8a024943f3d49ccd361424d357486d4df7841
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-03-14 17:20:25 -0700

    cleanup names

--- src/main/java/com/bina/lrsim/pb/ReadsWriter.java
+++ src/main/java/com/bina/lrsim/pb/ReadsWriter.java
@@ -13,17 +13,17 @@ import java.util.List;
  * Created by bayolau on 1/8/16.
  */
 public abstract class ReadsWriter implements Closeable {
-  protected final String filename_;
-  protected final String moviename_;
-  protected final int firsthole_;
+  protected final String filename;
+  protected final String moviename;
+  protected final int firsthole;
   protected final PBSpec spec;
-  private final List<Locus> loci_;
+  private final List<Locus> loci;
 
   public ReadsWriter(PBSpec spec, String filename, String moviename, int firsthole) {
-    this.loci_ = new ArrayList<>();
-    this.filename_ = filename;
-    this.moviename_ = moviename;
-    this.firsthole_ = firsthole;
+    this.loci = new ArrayList<>();
+    this.filename = filename;
+    this.moviename = moviename;
+    this.firsthole = firsthole;
     this.spec = spec;
   }
 
@@ -31,12 +31,12 @@ public abstract class ReadsWriter implements Closeable {
   public abstract void close() throws IOException;
 
   public final void addLocus(Locus locus) {
-    this.loci_.add(locus);
+    loci.add(locus);
   }
 
   public final void writeLociBed(String prefix, String moviename, int firsthole) {
     boolean writing = false;
-    for (Locus entry : this.loci_) {
+    for (Locus entry : loci) {
       if (null != entry) {
         writing = true;
         break;
@@ -45,7 +45,7 @@ public abstract class ReadsWriter implements Closeable {
     if (!writing) return;
     try (FileWriter fw = new FileWriter(new File(prefix + ".bed"))) {
       int shift = 0;
-      for (Locus entry : this.loci_) {
+      for (Locus entry : loci) {
         if (null != entry) {
           writeBedLine(fw, moviename + "/" + String.valueOf(firsthole + shift), entry);
         }
