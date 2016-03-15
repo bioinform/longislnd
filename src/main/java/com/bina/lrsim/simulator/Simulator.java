d7e16ac29311260e50ef51877c57ba7239a12680
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-03-15 15:38:08 -0700

    name cleanup

--- src/main/java/com/bina/lrsim/simulator/Simulator.java
+++ src/main/java/com/bina/lrsim/simulator/Simulator.java
@@ -1,5 +1,6 @@
 package com.bina.lrsim.simulator;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -26,10 +27,10 @@ import com.bina.lrsim.util.Monitor;
  */
 public class Simulator {
   private final static Logger log = Logger.getLogger(Simulator.class.getName());
-  private final long[] base_counter_ = new long[EnumEvent.values().length];
-  private final long[] event_counter_ = new long[EnumEvent.values().length];
-  final ConcurrentHashMap<String, AtomicLong> name_counter = new ConcurrentHashMap<String, AtomicLong>();
-  private RandomFragmentGenerator seqGen_;
+  private final long[] baseCounter = new long[EnumEvent.values().length];
+  private final long[] eventCounter = new long[EnumEvent.values().length];
+  final ConcurrentHashMap<String, AtomicLong> nameCounter = new ConcurrentHashMap<>();
+  private RandomFragmentGenerator seqGen;
 
   /**
    * Constructor
@@ -37,25 +38,25 @@ public class Simulator {
    * @param seqGen a random sequence generator
    */
   public Simulator(RandomFragmentGenerator seqGen) {
-    seqGen_ = seqGen;
+    this.seqGen = seqGen;
   }
 
   /**
    * Generate a pacbio h5 file containing reads simulated according to the sampler and reference
    * 
    * @param path output path
-   * @param movie_name movie name
+   * @param movieName movie name
    * @param firsthole first hole producing sequence
    * @param drawer an instance from which samples can be drawn
-   * @param total_bases minimum number of bp to generate
+   * @param totalBases minimum number of bp to generate
    * @param gen random number generator
    */
-  public int simulate(final String path, final String movie_name, final int firsthole, SamplesDrawer drawer, int total_bases, final Spec spec, RandomGenerator gen) throws IOException {
-    try (ReadsWriter writer = ReadsWriterFactory.makeWriter(spec, path + "/" + movie_name + spec.getSuffix(), movie_name, firsthole)) {
+  public int simulate(final String path, final String movieName, final int firsthole, SamplesDrawer drawer, int totalBases, final Spec spec, RandomGenerator gen) throws IOException {
+    try (ReadsWriter writer = ReadsWriterFactory.makeWriter(spec, new File(path, movieName + spec.getSuffix()).getPath(), movieName, firsthole)) {
       PBReadBuffer read = new PBReadBuffer(spec);
       log.info("generating reads");
 
-      for (int num_bases = 0; num_bases < total_bases;) {
+      for (int numBases = 0; numBases < totalBases;) {
         read.clear();
         if (read.size() != 0) {
           log.info("couldn't clear buffer");
@@ -63,82 +64,82 @@ public class Simulator {
         }
 
         // draw a list of smrt belts
-        Pair<int[], Integer> len_score = drawer.getRandomLengthScore(gen);
-        final int[] insert_lengths = len_score.getFirst();
-        int max_len = -1;
-        for (int len : insert_lengths) {
-          if (len > max_len) max_len = len;
+        Pair<int[], Integer> lenScore = drawer.getRandomLengthScore(gen);
+        final int[] insertLengths = lenScore.getFirst();
+        int maxLen = -1;
+        for (int len : insertLengths) {
+          if (len > maxLen) maxLen = len;
         }
 
-        final Fragment fragment = seqGen_.getFragment(max_len, gen);
+        final Fragment fragment = seqGen.getFragment(maxLen, gen);
         final Locus locus = fragment.getLocus();
-        name_counter.putIfAbsent(locus.getChrom(), new AtomicLong((long) 0));
-        name_counter.get(locus.getChrom()).incrementAndGet();
+        nameCounter.putIfAbsent(locus.getChrom(), new AtomicLong((long) 0));
+        nameCounter.get(locus.getChrom()).incrementAndGet();
         final byte[] sequence = fragment.getSeq();
         // correct insert lengths if the drawn fragment is shorter, the fractional change might not be realistic, but it avoids crazy coverage in fragment mode
-        if (sequence.length < max_len) {
-          final float ratio = (float) sequence.length / (float) max_len;
-          max_len = sequence.length;
-          for (int ii = 0; ii < insert_lengths.length; ++ii) {
-            insert_lengths[ii] *= ratio;
+        if (sequence.length < maxLen) {
+          final float ratio = (float) sequence.length / (float) maxLen;
+          maxLen = sequence.length;
+          for (int ii = 0; ii < insertLengths.length; ++ii) {
+            insertLengths[ii] *= ratio;
           }
         }
 
         // draw a sequence according to max insert length, make RC if belt is long enough
-        final List<byte[]> fw_rc = new ArrayList<>(2);
-        fw_rc.add(sequence);
-        if (insert_lengths.length > 1) {
-          final byte[] fw = fw_rc.get(0);
+        final List<byte[]> fwRc = new ArrayList<>(2);
+        fwRc.add(sequence);
+        if (insertLengths.length > 1) {
+          final byte[] fw = fwRc.get(0);
           final byte[] rc = new byte[fw.length];
           for (int pos = 0, tgt = fw.length - 1; pos < fw.length; ++pos, --tgt) {
             rc[tgt] = EnumBP.ascii_rc(fw[pos]);
           }
-          fw_rc.add(rc);
+          fwRc.add(rc);
         }
 
-        final List<Locus> clr_loci = new ArrayList<>();
+        final List<Locus> clrLoci = new ArrayList<>();
 
-        final List<Integer> section_ends = new ArrayList<>(2 * insert_lengths.length - 1);
+        final List<Integer> sectionEnds = new ArrayList<>(2 * insertLengths.length - 1);
         boolean skipIfShort = false;
-        for (int ins_idx = 0; ins_idx < insert_lengths.length; ++ins_idx) {
-          final int insert_length = insert_lengths[ins_idx];
-          final boolean first_clr = ins_idx == 0;
-          final boolean last_clr = ins_idx + 1 == insert_lengths.length;
-          final int begin = first_clr ? max_len - insert_length : 0;
-          final int end = last_clr ? insert_length : max_len;
-          final boolean isShort = insert_length < Heuristics.SMRT_INSERT_FRACTION * max_len && !first_clr && !last_clr;
+        for (int insIdx = 0; insIdx < insertLengths.length; ++insIdx) {
+          final int insertLength = insertLengths[insIdx];
+          final boolean firstClr = insIdx == 0;
+          final boolean lastClr = insIdx + 1 == insertLengths.length;
+          final int begin = firstClr ? maxLen - insertLength : 0;
+          final int end = lastClr ? insertLength : maxLen;
+          final boolean isShort = insertLength < Heuristics.SMRT_INSERT_FRACTION * maxLen && !firstClr && !lastClr;
           if (!isShort || !skipIfShort) {
-            if (!first_clr) {
+            if (!firstClr) {
               // prepend with a "perfect" adaptor sequence
               read.addASCIIBases(Heuristics.SMRT_ADAPTOR_STRING, Heuristics.SMRT_ADAPTOR_STRING, Heuristics.SMRT_ADAPTOR_SCORE);
-              section_ends.add(read.size());
+              sectionEnds.add(read.size());
             }
             AppendState deletion = null;
-            for (Iterator<Context> itr = new HPIterator(fw_rc.get(ins_idx % 2), begin, end, drawer.getLeftFlank(), drawer.getRightFlank(), drawer.getHpAnchor()); itr.hasNext();) {
+            for (Iterator<Context> itr = new HPIterator(fwRc.get(insIdx % 2), begin, end, drawer.getLeftFlank(), drawer.getRightFlank(), drawer.getHpAnchor()); itr.hasNext();) {
               final Context con = itr.next();
               if (null != con) {
-                deletion = drawer.appendTo(read, con, deletion, gen, base_counter_);
+                deletion = drawer.appendTo(read, con, deletion, gen, baseCounter);
               }
             }
 
             //per clr coordinates
-            int clr_begin = locus.getBegin0();
-            int clr_end = locus.getEnd0();
-            final boolean read_is_rc = (ins_idx % 2 == 1) ^ locus.isRc();
-            if (first_clr) {
-              if(read_is_rc)
-                clr_end = locus.getBegin0() + insert_length;
+            int clrBegin = locus.getBegin0();
+            int clrEnd = locus.getEnd0();
+            final boolean readIsRc = (insIdx % 2 == 1) ^ locus.isRc();
+            if (firstClr) {
+              if(readIsRc)
+                clrEnd = locus.getBegin0() + insertLength;
               else
-                clr_begin = locus.getEnd0() - insert_length;
+                clrBegin = locus.getEnd0() - insertLength;
             }
-            if (last_clr) {
-              if(read_is_rc)
-                clr_begin = locus.getEnd0() - insert_length;
+            if (lastClr) {
+              if(readIsRc)
+                clrBegin = locus.getEnd0() - insertLength;
               else
-                clr_end = locus.getBegin0() + insert_length;
+                clrEnd = locus.getBegin0() + insertLength;
             }
-            clr_loci.add(new Locus(locus.getChrom(), clr_begin, clr_end, read_is_rc));
-            section_ends.add(read.size());
+            clrLoci.add(new Locus(locus.getChrom(), clrBegin, clrEnd, readIsRc));
+            sectionEnds.add(read.size());
             if (isShort) {
               skipIfShort = true;
             }
@@ -148,8 +149,8 @@ public class Simulator {
           }
         }
 
-        writer.addLast(read, section_ends, len_score.getSecond(), locus, clr_loci);
-        num_bases += read.size();
+        writer.addLast(read, sectionEnds, lenScore.getSecond(), locus, clrLoci);
+        numBases += read.size();
         if (writer.size() % 10000 == 1) {
           log.info(toString());
         }
@@ -165,11 +166,11 @@ public class Simulator {
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("simulated statistics\n");
-    sb.append(EnumEvent.getPrettyStats(base_counter_));
+    sb.append(EnumEvent.getPrettyStats(baseCounter));
     sb.append("\n");
-    sb.append(EnumEvent.getPrettyStats(event_counter_));
+    sb.append(EnumEvent.getPrettyStats(eventCounter));
     sb.append("\n");
-    for (ConcurrentHashMap.Entry<String, AtomicLong> entry : name_counter.entrySet()) {
+    for (ConcurrentHashMap.Entry<String, AtomicLong> entry : nameCounter.entrySet()) {
       sb.append(entry.getKey());
       sb.append(" ");
       sb.append(entry.getValue().get());
