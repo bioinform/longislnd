cc292339ec76a68fa4ebe126e31cee4d628572f6
Marghoob Mohiyuddin <marghoob.mohiyuddin@bina.roche.com>
2016-03-16 14:36:52 -0700

    refactor

--- src/main/java/com/bina/lrsim/pb/h5/bax/EnumTypeIdx.java
+++ src/main/java/com/bina/lrsim/pb/h5/bax/EnumTypeIdx.java
@@ -1,13 +1,26 @@
 package com.bina.lrsim.pb.h5.bax;
 
+import java.util.Arrays;
+
 /**
  * Created by bayo on 5/6/15.
  */
 public enum EnumTypeIdx {
   TypeAdapter("Adapter"), TypeInsert("Insert"), TypeHQRegion("HQRegion");
 
+  public static final String[] descriptionArray;
+
+  static {
+    descriptionArray = new String[values().length];
+    int i = 0;
+    for (EnumTypeIdx enumTypeIdx : values()) {
+      descriptionArray[i] = enumTypeIdx.description;
+      i++;
+    }
+  }
+
   static String[] getDescriptionArray() {
-    return new String[] {TypeAdapter.description, TypeInsert.description, TypeHQRegion.description};
+    return descriptionArray;
   }
 
   EnumTypeIdx(final String description) {
