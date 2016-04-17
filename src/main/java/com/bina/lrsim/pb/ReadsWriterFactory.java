007ba06c5fb5511c1a6abc975a47e188d2061b0a
Bayo Lau <bayo.lau@gmail.com>
2016-04-16 21:59:11 -0700

    expose fastq writer

--- src/main/java/com/bina/lrsim/pb/ReadsWriterFactory.java
+++ src/main/java/com/bina/lrsim/pb/ReadsWriterFactory.java
@@ -1,5 +1,6 @@
 package com.bina.lrsim.pb;
 
+import com.bina.lrsim.fastq.FastqWriter;
 import com.bina.lrsim.pb.h5.bax.BaxH5Writer;
 import com.bina.lrsim.pb.bam.BAMWriter;
 import htsjdk.samtools.BamFileIoUtils;
@@ -11,6 +12,12 @@ import java.io.File;
  */
 public class ReadsWriterFactory {
   public static ReadsWriter makeWriter(Spec spec, String fileName, String movieName, int firstHole, RunInfo runInfo) {
-    return BamFileIoUtils.isBamFile(new File(fileName)) ? new BAMWriter(spec, fileName, movieName, firstHole, runInfo) : new BaxH5Writer(spec, fileName, movieName, firstHole, runInfo);
+    if (fileName.endsWith(".fastq") || fileName.endsWith(".fq")) {
+      return new FastqWriter(spec, fileName, movieName, firstHole, runInfo);
+    } else if (BamFileIoUtils.isBamFile(new File(fileName))) {
+      return new BAMWriter(spec, fileName, movieName, firstHole, runInfo);
+    } else {
+      return new BaxH5Writer(spec, fileName, movieName, firstHole, runInfo);
+    }
   }
 }
