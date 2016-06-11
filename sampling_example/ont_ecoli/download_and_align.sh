aee08f7837170b37a9c7c1aae21f0f0271066220
Bayo Lau <bayo.lau@gmail.com>
2016-06-10 23:34:30 -0700

    added alignment goodies

--- sampling_example/ont_ecoli/download_and_align.sh
+++ sampling_example/ont_ecoli/download_and_align.sh
@@ -13,10 +13,25 @@ curl ftp://ftp.sra.ebi.ac.uk/vol1/ERA411/ERA411499/oxfordnanopore_native/flowcel
 #ERX708231
 curl ftp://ftp.sra.ebi.ac.uk/vol1/ERA411/ERA411499/oxfordnanopore_native/flowcell_39.tar | tar x
 
+declare -a FOLDERS=(flowcell_20 flowcell_32 flowcell_33 flowcell_39_K12_Histag)
+
 #extract fastq
 JVM_OPT=-Djava.library.path=../../build/lib
 JAR=../../LRSim.jar
-java ${JVM_OPT} -jar ${JAR} fast5extract fastq flowcell_20
-java ${JVM_OPT} -jar ${JAR} fast5extract fastq flowcell_32
-java ${JVM_OPT} -jar ${JAR} fast5extract fastq flowcell_33
-java ${JVM_OPT} -jar ${JAR} fast5extract fastq flowcell_39_K12_Histag
+for FOLDER in "${FOLDERS[@]}"
+do
+  java ${JVM_OPT} -jar ${JAR} fast5extract fastq ${FOLDER}
+done
+
+#download reference
+REF=GCF_000005845.2_ASM584v2_genomic.fa
+curl ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz | gunzip > ${REF}
+SAMTOOLS=../samtools-1.2/samtools
+${SAMTOOLS} faidx ${REF}
+
+for FOLDER in "${FOLDERS[@]}"
+do
+  FASTQ=${FOLDER}.fastq
+  ../graphmap/bin/Linux-x64/graphmap align -r ${REF} -d ${FASTQ} | ${SAMTOOLS} view -Shu - | ${SAMTOOLS} sort - ${FASTQ}
+  ${SAMTOOLS} index ${FASTQ}.bam
+done
