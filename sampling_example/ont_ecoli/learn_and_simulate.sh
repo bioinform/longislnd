599087d2b827161323ef3a1e87b87c3094920ab9
Bayo Lau <bayo.lau@gmail.com>
2016-06-13 22:29:26 -0700

    learn and simulate example for ont

---
+++ sampling_example/ont_ecoli/learn_and_simulate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+../../sample.py --input_suffix fastq.bam --read_type fastq --model_dir model --flank 3 --reference GCF_000005845.2_ASM584v2_genomic.fa
+../../simulate.py --read_type fastq --model_dir model --fasta GCF_000005845.2_ASM584v2_genomic.fa
