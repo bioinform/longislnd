3d69961bcf218ea8dc09589b4ee7c7515210adca
Yunfei Guo <yunfei.guo@bina.roche.com>
2016-11-08 13:44:31 -0800

    add some comments for better understanding.

--- src/main/java/com/bina/lrsim/bioinfo/PairwiseAlignment.java
+++ src/main/java/com/bina/lrsim/bioinfo/PairwiseAlignment.java
@@ -20,10 +20,19 @@ public class PairwiseAlignment {
     this.seqDataIdx = seqDataIdx;
   }
 
+  /**
+   *
+   * @param fixed
+   * @param movable
+   * @param indices
+   * @param leftMostTarget inclusive left boundary
+   * @param rightMostTarget exclusive right boundary
+   */
   private static void spanLeftOnMatching(final byte[] fixed, final byte[] movable, final int[] indices, int leftMostTarget, int rightMostTarget) {
     if (fixed == movable) return;
     for (int pos = leftMostTarget; pos < rightMostTarget; ++pos) {
       final byte base = movable[pos];
+      //skip gaps/spaces (could be insertions/deletions)
       if (base == EnumBP.Gap.ascii || EnumBP.ascii2value(base) == EnumBP.N.value) continue;
       int leftMostMatch = pos;
       for (int leftCandidate = leftMostMatch - 1; leftCandidate >= leftMostTarget && movable[leftCandidate] == EnumBP.Gap.ascii; --leftCandidate) {
@@ -31,6 +40,26 @@ public class PairwiseAlignment {
           leftMostMatch = leftCandidate;
         }
       }
+      /* an example of what this method is doing
+      fixed:    CCGAACGAA--
+      movable:  CC----GAATT
+
+      ||
+      \/
+      fixed:    CCGAACGAA--
+      movable:  CCG----AATT
+      the match G is moved to the leftmost position by skipping some gaps
+
+      ||
+      \/
+      fixed:    CCGAACGAA--
+      movable:  CCG-A---ATT
+      the match A which was next to G will be moved to the left, but a
+      spacing is kept between two adjacent moved matches. as a result,
+      gaps will be dispersed rather than clustered. The purpose is to
+      avoid alignment bias.
+
+       */
       if (leftMostMatch != pos) {
         movable[pos] = EnumBP.Gap.ascii;
         movable[leftMostMatch] = base;
@@ -75,8 +104,10 @@ public class PairwiseAlignment {
     // if there are soft-clips, the left and right ends will have long stretches of non-gap seq
     // the boundaries prevent spanning outside of the first and last non-gap ref
     int leftBoundary = 0;
+    // locate the last upstream softclipped position in reference
     for (; leftBoundary < size() && ref[leftBoundary] == EnumBP.Gap.ascii; ++leftBoundary) {}
     int rightBoundary = size();
+    // locate the first downstream softclipped position in reference
     for (; rightBoundary > 0 && ref[rightBoundary - 1] == EnumBP.Gap.ascii; --rightBoundary) {}
     // log.info(new String(seq_));
     // log.info(new String(ref_));
@@ -115,20 +146,33 @@ public class PairwiseAlignment {
       final byte nextBase = ref[pos + 1];
       final byte next2Base = ref[pos + 2];
       if (base == EnumBP.Gap.ascii && seq[pos] != EnumBP.Gap.ascii) {
-        // AC G --> AC G
-        // A TG --> AT G
+        /*
+        an example for the change
+                   *
+        // seq:   AC G --> AC G
+        // ref:   A TG --> AT G
+        net effect is changing one insertion + one deletion to one mismatch
         // p
-        // CC T --> CC T
-        // C TT --> C TT
+        // seq:   CC T --> CC T
+        // ref:   C TT --> C TT
+        note here seq[pos] != lastBase && nextBase != next2Base
+        the second case is a situation where indels will not be merged, the reason is not merging indels in homopolymers.
+        */
         if (lastBase != EnumBP.Gap.ascii && lastBase == seq[pos - 1] && seq[pos] != lastBase && nextBase != EnumBP.Gap.ascii && nextBase != next2Base && seq[pos + 1] == EnumBP.Gap.ascii && next2Base != EnumBP.Gap.ascii && next2Base == seq[pos + 2]) {
           ref[pos] = nextBase;
           ref[pos + 1] = EnumBP.Gap.ascii;
         }
-        // A CG --> A CG
-        // AT G --> A TG
+        /*
+        // an example for the change
+                    *
+        // seq:   A CG --> A CG
+        // ref:   AT G --> A TG
+        net effect is changing one insertion + one deletion to one mismatch
         // p
-        // C TT --> C TT
-        // CC T --> CC T
+        // seq:   C TT --> C TT
+        // ref:   CC T --> CC T
+        // note here last2Base == seq[pos - 2] && lastBase != last2Base
+        */
         else if (last2Base != EnumBP.Gap.ascii && last2Base == seq[pos - 2] && lastBase != EnumBP.Gap.ascii && lastBase != last2Base && seq[pos - 1] == EnumBP.Gap.ascii && seq[pos] != nextBase && nextBase != EnumBP.Gap.ascii && nextBase == seq[pos + 1]) {
           ref[pos - 1] = EnumBP.Gap.ascii;
           ref[pos] = lastBase;
@@ -150,6 +194,10 @@ public class PairwiseAlignment {
         rightMostGap = rightCanadidate;
       }
 
+      /*
+      if we can find a match, then we right shift the matched base
+      if we can't we right shift to the last gap
+       */
       if (rightMostMatch != pos) {
         ref[pos] = EnumBP.Gap.ascii;
         ref[rightMostMatch] = base;
